5c4d393961d7fc7e4b0e5b96e8c810b906d21555
s-okayama <sohei.okayama@uzabase.com>
2024-08-31 19:49:40 +0900

    commitはみちゃだめ＞＜

--- user-api/build_and_push.sh
+++ user-api/build_and_push.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sample.delta.jdbc.driverClassName=org.postgresql.Driver
+# postgres version 11.7
 sample.delta.jdbc.url=jdbc:postgresql://localhost:5432/users?currentSchema=public
 sample.delta.jdbc.username=developer
 sample.delta.jdbc.password=KBEeD2YD7L
